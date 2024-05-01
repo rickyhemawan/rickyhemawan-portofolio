ab9e8e4411163f3dd4b70c8fbee989b2ef147cb8
rickyhemawan <rickyhemawan23@gmail.com>
2024-05-01 13:41:03 +0800

    feat: added card placeholder

---
+++ src/app/components/card-vertical.tsx
@@ -0,0 +1,32 @@
+const CardVertical = () => {
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white bg-opacity-15 mx-2">
+      <img
+        className="w-full"
+        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+        alt="Sunset in the mountains"
+      />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+        <p className="text-base text-justify">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+          quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
+          nihil.
+        </p>
+      </div>
+      <div className="px-6 pt-4 pb-2">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          #photography
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          #travel
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          #winter
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default CardVertical
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,30 @@
 import AppNavbar from './components/app-navbar'
+import CardVertical from './components/card-vertical'
+import MdxLayout from './components/mdx-layout'
+
 import PlaceholderText from '../contents/placeholder-text.mdx'
 import AchievementText from '../contents/achievements.mdx'
-import MdxLayout from './components/mdx-layout'
 
 export default function Home() {
   return (
     <main>
       <AppNavbar />
       <div className="mx-12 my-12">
+        {/* card sandbox */}
+        <div className="flex">
+          <div className="flex-1">
+            <CardVertical />
+          </div>
+          <div className="flex-1">
+            <CardVertical />
+          </div>
+          <div className="flex-1">
+            <CardVertical />
+          </div>
+        </div>
+        {/* divider */}
+        <div className="mt-80"></div>
+        {/* text sandbox */}
         <div className="grid md:grid-cols-2 sm:grid-cols-1">
           <div className="md:mx-2 flex flex-col">
             <h1 className="text-5xl font-bold ">Ricky Hemawan</h1>
