827ff5eae93045ba24eb2ddd2ec4061c4acbc914
rickyhemawan <rickyhemawan23@gmail.com>
2024-05-06 02:24:50 +0800

    feat: added exit animation for cards list

--- src/app/sections/skillset/components/cards-container.tsx
+++ src/app/sections/skillset/components/cards-container.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import { useState } from 'react'
-import { motion } from 'framer-motion'
+import { useEffect, useRef, useState } from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
 import CardReadMore from './card-read-more'
 import CardGradientBorder from '@/app/components/card-gradient-border'
 
@@ -10,51 +10,51 @@ export interface Content {
   content: React.ReactNode
 }
 
+// TODO: fix bug when "testing" content read more pressed, it will scroll down to next content
 const CardsContainer = ({ contents }: { contents: Content[] }) => {
   const [content, setContent] = useState<Content | null>(null)
 
   const handleButtonClick = (content: Content) => {
-    console.log('button clicked', content)
     setContent(content)
   }
 
-  if (content !== null) {
-    return (
-      <div className="flex flex-wrap justify-center gap-2">
-        <motion.div
-          key={content.key}
-          initial={{ width: 0, opacity: 0 }}
-          animate={{ width: '100%', opacity: 1 }}
-          transition={{ duration: 1 }}
-        >
+  return (
+    <motion.div
+      className="flex flex-wrap justify-center gap-2"
+      style={{ originX: 0 }}
+    >
+      <AnimatePresence>
+        {content === null ? (
+          contents.map(content => {
+            return (
+              <motion.div
+                key={content.key}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ height: 0, opacity: 0 }}
+                transition={{ duration: 0.5 }}
+                className="w-full max-w-[calc(66.666%_-_4px)] md:max-w-[calc(33.333%_-_4px)] lg:max-w-[calc(25%_-_4px)]"
+              >
+                <CardReadMore onButtonClick={() => handleButtonClick(content)}>
+                  {content.content}
+                </CardReadMore>
+              </motion.div>
+            )
+          })
+        ) : (
           <motion.div
-            initial={{ height: '18rem', overflow: 'hidden' }}
-            animate={{ height: '100%' }}
-            transition={{ duration: 0.5, delay: 0.5 }}
+            key={`selected-${content.key}`}
+            className="w-full"
+            initial={{ height: 0, opacity: 0 }}
+            animate={{ height: '100%', opacity: 1 }}
+            transition={{ duration: 1, type: 'tween' }}
           >
-            <CardGradientBorder className="h-full">
-              {content.content}
+            <CardGradientBorder className="h-full w-full">
+              <div className="min-h-8">{content?.content}</div>
             </CardGradientBorder>
           </motion.div>
-        </motion.div>
-      </div>
-    )
-  }
-
-  return (
-    <motion.div className="flex flex-wrap justify-center gap-2">
-      {contents.map(content => {
-        return (
-          <motion.div
-            key={content.key}
-            className="w-full max-w-[calc(66.666%_-_4px)] md:max-w-[calc(33.333%_-_4px)] lg:max-w-[calc(25%_-_4px)]"
-          >
-            <CardReadMore onButtonClick={() => handleButtonClick(content)}>
-              {content.content}
-            </CardReadMore>
-          </motion.div>
-        )
-      })}
+        )}
+      </AnimatePresence>
     </motion.div>
   )
 }
