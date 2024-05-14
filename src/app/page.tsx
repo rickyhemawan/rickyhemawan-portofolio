a7e6ab63788cbdfe0028319d96191c2a73b6a20e
rickyhemawan <rickyhemawan23@gmail.com>
2024-05-14 20:01:25 +0800

    feat: change page top margin to 10vh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       {/* TODO: fix Theme colors before unlocking this feature */}
       {/* <ThemeSwitcher /> */}
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <IntroductionSection className="mt-[10vh] pb-24 md:mt-[20vh]" />
+        <IntroductionSection className="mt-[10vh] pb-24" />
         {/* card sandbox */}
         <AchievementsSection className="py-24" />
       </div>
