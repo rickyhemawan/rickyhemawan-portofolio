18b776c95626b2d6517123e71fc02706ddd6a206
rickyhemawan <rickyhemawan23@gmail.com>
2024-05-18 11:26:19 +0800

    feat: added navigation-ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import IntroductionSection from './sections/introduction'
 import SkillsetSection from './sections/skillset'
 import AchievementsSection from './sections/achievements'
+import FluidNavbar from './components/fluid-navbar'
 
 export default function Home() {
   return (
@@ -9,9 +10,10 @@ export default function Home() {
       {/* TODO: fix Theme colors before unlocking this feature */}
       {/* <ThemeSwitcher /> */}
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <IntroductionSection className="mt-[10vh] pb-24" />
+        <IntroductionSection className="mt-[10vh] pb-20" />
+        <FluidNavbar />
         {/* card sandbox */}
-        <AchievementsSection className="py-24" />
+        <AchievementsSection className="pb-24 pt-4" />
       </div>
     </main>
   )
